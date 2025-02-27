7bf11e607f9c1579f8f306d3aef14eb242a78332
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-02-27 14:57:51 +0000

    created view for driver to check history

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/passenger/reservations', function () {
+    return view('passenger.reservations');
 });
 
 Route::get('/dashboard', function () {
