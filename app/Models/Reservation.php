2a182e6ba699cb229cd1a1aad34bef9d4134f41b
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-02-28 20:11:19 +0000

    added relationships to reservation model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Driver;
+use App\Models\User;
+use App\Models\Trajet;
+
 class Reservation extends Model
 {
     protected $table = 'reservation';
@@ -12,4 +16,16 @@ class Reservation extends Model
         'driver_id',
         'passenger_id'
     ];
+
+    public function driver(){
+        return $this->belongsTo(Driver::class, 'driver_id');
+    }
+
+    public function passenger(){
+        return $this->belongsTo(User::class, 'passenger_id');
+    }
+
+    public function trajet(){
+        return $this->hasOne(Trajet::class, 'reservation_id');
+    }
 }
